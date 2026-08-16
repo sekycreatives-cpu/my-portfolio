d82cb0013464881f50ab6b8e19433f4aab097898
sekycreatives-cpu <sekycreatives@gmail.com>
2026-08-17 00:24:46 +0530

    Add files via upload

--- src/components/ShowcaseMarqueeGridSection.tsx
+++ src/components/ShowcaseMarqueeGridSection.tsx
@@ -7,64 +7,42 @@ interface ShowcaseItem {
   image: string;
 }
 
-// ROW 1: Right -> Left (Branding & Social Media mix)
+// ROW 1: Right -> Left (Branding & Creative Campaign mix)
 const ROW_1: ShowcaseItem[] = [
-  { id: "r1-new-1", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786689786/post_4_copy_s0dnc9.jpg" },
-  { id: "r1-1", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1785846217/Brand01_01_gq8k9g.png" },
-  { id: "r1-2", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441150/Cycling_4_o74asg.jpg" },
-  { id: "r1-3", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786220085/Flynet_Rebrand_copy_2_gqevmw.jpg" },
-  { id: "r1-4", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441149/square_post_2_jkffyx.jpg" },
-  { id: "r1-5", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786724030/Robinson_main_2_psjqgv.png" },
-  { id: "r1-6", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441148/3_ek624q.jpg" },
-  { id: "r1-7", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1785870642/Brand03_1.1_yxuvw2.png" },
-  { id: "r1-8", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441147/Adventure_oimbth.jpg" },
-  { id: "r1-9", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1785858983/Brand01_04_uxobti.png" },
-  { id: "r1-10", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441146/Post_22_mf6nbo.jpg" },
+  { id: "r1-c1", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786689786/Post_7_jabfgu.jpg" },
+  { id: "r1-b1", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1785846217/Brand01_01_gq8k9g.png" },
+  { id: "r1-c2", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786441150/Cycling_4_o74asg.jpg" },
+  { id: "r1-b2", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786220085/Flynet_Rebrand_copy_2_gqevmw.jpg" },
+  { id: "r1-c3", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786441731/WhatsApp_Image_2026-08-11_at_3.17.19_PM_xbtram.jpg" },
+  { id: "r1-b3", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786724030/Robinson_main_2_psjqgv.png" },
+  { id: "r1-c4", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786441147/Adventure_oimbth.jpg" },
+  { id: "r1-b4", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1785870642/Brand03_1.1_yxuvw2.png" },
 ];
 
-// ROW 2: Left -> Right (Branding & Social Media mix)
+// ROW 2: Left -> Right (Branding & Creative Campaign mix)
 const ROW_2: ShowcaseItem[] = [
-  { id: "r2-new-1", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786689786/post_2-2_esq5k5.jpg" },
-  { id: "r2-1", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441150/White_l5lhfx.jpg" },
-  { id: "r2-2", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1785909837/Brand03_7_smj7jm.png" },
-  { id: "r2-3", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441148/Artboard_84_xa8qvj.jpg" },
-  { id: "r2-4", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1785867490/Brand02_2_r9zk6i.png" },
-  { id: "r2-5", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441146/Square_n3mugi.jpg" },
-  { id: "r2-6", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786220086/Flynet_Rebrand_copy_6_qctizo.jpg" },
-  { id: "r2-7", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441145/Square_size_hg1vkl.jpg" },
-  { id: "r2-8", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1785858027/Brand01_2_oayh4i.png" },
-  { id: "r2-9", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441142/Kaalisuwari_1_sm1k4g.jpg" },
-  { id: "r2-10", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786220140/Flynet_Rebrand_copy_7_qsyv4v.jpg" },
+  { id: "r2-c1", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786689786/post_2-2_esq5k5.jpg" },
+  { id: "r2-c2", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786441150/White_l5lhfx.jpg" },
+  { id: "r2-b1", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786721231/Milar_banner_hcckde.jpg" },
+  { id: "r2-c3", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786441729/WhatsApp_Image_2026-08-11_at_3.17.19_PM_2_gdleni.jpg" },
+  { id: "r2-b2", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1785867490/Brand02_2_r9zk6i.png" },
+  { id: "r2-c4", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786441151/Instagram_Mockup_fqokhi.jpg" },
+  { id: "r2-b3", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786220086/Flynet_Rebrand_copy_6_qctizo.jpg" },
+  { id: "r2-c5", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786441142/Kaalisuwari_1_sm1k4g.jpg" },
 ];
 
-// ROW 3: Right -> Left (Branding & Social Media mix)
+// ROW 3: Right -> Left (Branding & Creative Campaign mix)
 const ROW_3: ShowcaseItem[] = [
-  { id: "r3-new-1", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786689786/Post_7_jabfgu.jpg" },
-  { id: "r3-1", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786220085/Flynet_Rebrand_copy_4_nei34u.jpg" },
-  { id: "r3-2", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441141/Just_Wear4_wl8idv.jpg" },
-  { id: "r3-3", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1785871287/WhatsApp_Image_2026-08-05_at_12.12.55_AM_2_lpqtxg.jpg" },
-  { id: "r3-4", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441728/WhatsApp_Image_2026-08-11_at_3.17.18_PM_3_kswl4c.jpg" },
-  { id: "r3-5", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1785867489/Brand02_3_hrazvp.png" },
-  { id: "r3-6", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441729/WhatsApp_Image_2026-08-11_at_3.17.19_PM_2_gdleni.jpg" },
-  { id: "r3-7", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1785846220/Brand01_3_c2irra.png" },
-  { id: "r3-8", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441730/WhatsApp_Image_2026-08-11_at_3.17.20_PM_2_eznpfu.jpg" },
-  { id: "r3-9", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441147/Post_25_piwmcr.jpg" },
-  { id: "r3-10", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441731/WhatsApp_Image_2026-08-11_at_3.17.19_PM_xbtram.jpg" },
-];
-
-// ROW 4: Left -> Right (Branding & Social Media mix)
-const ROW_4: ShowcaseItem[] = [
-  { id: "r4-new-1", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786689786/Construction_Post_2_u476tl.jpg" },
-  { id: "r4-1", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441729/WhatsApp_Image_2026-08-11_at_3.17.19_PM_1_tfkxjd.jpg" },
-  { id: "r4-2", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1785870823/Brand03_2.1_sg0vhy.png" },
-  { id: "r4-3", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441728/WhatsApp_Image_2026-08-11_at_3.17.18_PM_yis2pt.jpg" },
-  { id: "r4-4", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1785867490/Brand02_4_sjfwfx.png" },
-  { id: "r4-5", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441729/WhatsApp_Image_2026-08-11_at_3.17.20_PM_1_aanvxy.jpg" },
-  { id: "r4-6", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441751/WhatsApp_Image_2026-08-11_at_3.17.17_PM_qetqug.jpg" },
-  { id: "r4-7", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441141/Hiring1_zltqzt.jpg" },
-  { id: "r4-8", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441751/WhatsApp_Image_2026-08-11_at_3.17.17_PM_1_fgryuh.jpg" },
-  { id: "r4-9", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441149/Artboard_83_wprr8i.jpg" },
-  { id: "r4-10", image: "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441730/WhatsApp_Image_2026-08-11_at_3.17.18_PM_2_qaqiig.jpg" },
+  { id: "r3-c1", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786441145/WhatsApp_Image_2025-01-27_at_4.38.18_PM_2_vjctph.jpg" },
+  { id: "r3-b1", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786220085/Flynet_Rebrand_copy_4_nei34u.jpg" },
+  { id: "r3-c2", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786441141/Just_Wear4_wl8idv.jpg" },
+  { id: "r3-b2", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786721828/Milar_Logo_Brand_Guidelines_Updated_pages-to-jpg-0004_yqsxur.jpg" },
+  { id: "r3-c3", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786441728/WhatsApp_Image_2026-08-11_at_3.17.18_PM_yis2pt.jpg" },
+  { id: "r3-c4", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786441142/WhatsApp_Image_2025-01-27_at_4.38.17_PM_1_l5hzdz.jpg" },
+  { id: "r3-c5", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786441143/WhatsApp_Image_2025-01-27_at_4.38.17_PM_ymxcb5.jpg" },
+  { id: "r3-c6", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786441141/WhatsApp_Image_2025-01-27_at_4.38.19_PM_u21p5e.jpg" },
+  { id: "r3-c7", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1786441751/WhatsApp_Image_2026-08-11_at_3.17.17_PM_qetqug.jpg" },
+  { id: "r3-b3", image: "https://res.cloudinary.com/eupac3wd/image/upload/v1785867490/Brand02_4_sjfwfx.png" },
 ];
 
 export default function ShowcaseMarqueeGridSection() {
@@ -91,7 +69,7 @@ export default function ShowcaseMarqueeGridSection() {
         </h2>
       </motion.div>
 
-      {/* CONTINUOUS MULTI-ROW MARQUEE WALL */}
+      {/* CONTINUOUS 3-ROW MARQUEE WALL */}
       <div className="w-full flex flex-col gap-0 relative">
         {/* ROW 1: RIGHT TO LEFT */}
         <MarqueeRow
@@ -107,23 +85,12 @@ export default function ShowcaseMarqueeGridSection() {
           speed={48}
         />
 
-        {/* ROW 3: RIGHT TO LEFT (Desktop & Tablet) */}
-        <div className="hidden sm:block">
-          <MarqueeRow
-            items={ROW_3}
-            direction="left"
-            speed={58}
-          />
-        </div>
-
-        {/* ROW 4: LEFT TO RIGHT (Desktop & Tablet) */}
-        <div className="hidden sm:block">
-          <MarqueeRow
-            items={ROW_4}
-            direction="right"
-            speed={52}
-          />
-        </div>
+        {/* ROW 3: RIGHT TO LEFT */}
+        <MarqueeRow
+          items={ROW_3}
+          direction="left"
+          speed={54}
+        />
       </div>
     </section>
   );
--- src/components/SocialMediaBentoGrid.tsx
+++ src/components/SocialMediaBentoGrid.tsx
@@ -1,79 +1,39 @@
 import { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "motion/react";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import { motion } from "motion/react";
 import ProgressiveImage from "./ProgressiveImage";
 
-// High Quality 1:1 Square Images for Social Media Gallery
+// High Quality 1:1 Square Images for Social Media Gallery (Exact 16 Selected Designs)
 const socialImages: string[] = [
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786689786/post_4_copy_s0dnc9.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786689786/post_2-2_esq5k5.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786689786/Post_7_jabfgu.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786689786/Construction_Post_2_u476tl.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441150/White_l5lhfx.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441150/Cycling_4_o74asg.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441149/Artboard_83_wprr8i.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441149/square_post_2_jkffyx.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441148/3_ek624q.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441148/WhatsApp_Image_2025-01-27_at_4.38.20_PM_ctv0yg.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441148/Artboard_84_xa8qvj.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441147/Post_25_piwmcr.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441147/Adventure_oimbth.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441147/WhatsApp_Image_2025-01-27_at_4.38.16_PM_1_wpd6z4.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441146/Post_22_mf6nbo.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441146/post_34_white_399_o3i7ek.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441146/Square_n3mugi.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441145/Square_size_hg1vkl.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441145/WhatsApp_Image_2025-01-27_at_4.38.18_PM_2_vjctph.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441144/WhatsApp_Image_2025-01-27_at_4.38.21_PM_2_kcohqb.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441145/WhatsApp_Image_2025-01-27_at_4.38.21_PM_b6sdih.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441144/WhatsApp_Image_2025-01-27_at_4.38.21_PM_1_xxljeq.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441143/WhatsApp_Image_2025-01-27_at_4.38.17_PM_2_kost83.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441143/post2_2_jq7jk7.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441143/WhatsApp_Image_2025-01-27_at_4.38.17_PM_ymxcb5.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441143/WhatsApp_Image_2025-01-27_at_4.38.16_PM_djdmhy.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441142/WhatsApp_Image_2025-01-27_at_4.38.17_PM_1_l5hzdz.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441142/Post_11_eg6vqw.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441142/7_n6r7ka.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441142/WhatsApp_Image_2025-01-27_at_4.38.18_PM_1_qlcbop.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441141/Hiring1_zltqzt.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441142/nov_post_13_fjwpkv.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441142/Kaalisuwari_1_sm1k4g.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441141/WhatsApp_Image_2025-01-27_at_4.38.20_PM_1_lwo84b.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441141/WhatsApp_Image_2025-01-27_at_4.38.19_PM_u21p5e.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441141/Just_Wear4_wl8idv.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441141/WhatsApp_Image_2025-01-27_at_4.38.18_PM_vdux1t.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441728/WhatsApp_Image_2026-08-11_at_3.17.18_PM_3_kswl4c.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441729/WhatsApp_Image_2026-08-11_at_3.17.19_PM_2_gdleni.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441729/WhatsApp_Image_2026-08-11_at_3.17.19_PM_3_xwt7uo.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441729/WhatsApp_Image_2026-08-11_at_3.17.19_PM_1_tfkxjd.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441728/WhatsApp_Image_2026-08-11_at_3.17.18_PM_yis2pt.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441729/WhatsApp_Image_2026-08-11_at_3.17.20_PM_1_aanvxy.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441730/WhatsApp_Image_2026-08-11_at_3.17.20_PM_2_eznpfu.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441730/WhatsApp_Image_2026-08-11_at_3.17.18_PM_1_mtdfdq.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441729/WhatsApp_Image_2026-08-11_at_3.17.20_PM_3_g7srik.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441751/WhatsApp_Image_2026-08-11_at_3.17.17_PM_qetqug.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441731/WhatsApp_Image_2026-08-11_at_3.17.19_PM_xbtram.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441730/WhatsApp_Image_2026-08-11_at_3.17.18_PM_2_qaqiig.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441751/WhatsApp_Image_2026-08-11_at_3.17.17_PM_2_dlg3db.jpg",
-  "https://res.cloudinary.com/eupac3wd/image/upload/f_auto,q_auto/v1786441751/WhatsApp_Image_2026-08-11_at_3.17.17_PM_1_fgryuh.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786689786/Post_7_jabfgu.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786441731/WhatsApp_Image_2026-08-11_at_3.17.19_PM_xbtram.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786441729/WhatsApp_Image_2026-08-11_at_3.17.19_PM_2_gdleni.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786441728/WhatsApp_Image_2026-08-11_at_3.17.18_PM_yis2pt.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786689786/post_2-2_esq5k5.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786441150/White_l5lhfx.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786441150/Cycling_4_o74asg.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786441151/Instagram_Mockup_fqokhi.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786441147/Adventure_oimbth.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786441145/WhatsApp_Image_2025-01-27_at_4.38.18_PM_2_vjctph.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786441142/WhatsApp_Image_2025-01-27_at_4.38.17_PM_1_l5hzdz.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786441142/Kaalisuwari_1_sm1k4g.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786441143/WhatsApp_Image_2025-01-27_at_4.38.17_PM_ymxcb5.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786441141/WhatsApp_Image_2025-01-27_at_4.38.19_PM_u21p5e.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786441141/Just_Wear4_wl8idv.jpg",
+  "https://res.cloudinary.com/eupac3wd/image/upload/v1786441751/WhatsApp_Image_2026-08-11_at_3.17.17_PM_qetqug.jpg",
 ];
 
-const INITIAL_VISIBLE_COUNT = 12;
-
 function useColumnCount() {
-  const [cols, setCols] = useState<number>(6);
+  const [cols, setCols] = useState<number>(4);
 
   useEffect(() => {
     const updateCols = () => {
       const width = window.innerWidth;
       if (width < 640) {
         setCols(2);
-      } else if (width < 768) {
+      } else if (width < 1024) {
         setCols(3);
-      } else if (width < 1280) {
-        setCols(4);
       } else {
-        setCols(6);
+        setCols(4);
       }
     };
 
@@ -86,10 +46,9 @@ function useColumnCount() {
 }
 
 export default function SocialMediaBentoGrid() {
-  const [isExpanded, setIsExpanded] = useState<boolean>(false);
   const columnCount = useColumnCount();
 
-  // Distribute all images into column arrays for left-to-right top-aligned masonry
+  // Distribute all 16 images into column arrays for left-to-right top-aligned masonry
   const columns = Array.from({ length: columnCount }, () => [] as { url: string; index: number }[]);
   socialImages.forEach((url, idx) => {
     const colIndex = idx % columnCount;
@@ -112,80 +71,47 @@ export default function SocialMediaBentoGrid() {
           <div className="flex items-center gap-2">
             <span className="w-2 h-2 rounded-full bg-[#FE4A03] animate-pulse" />
             <span className="text-[#FE4A03] font-mono text-[11px] sm:text-xs tracking-widest uppercase font-semibold">
-              SOCIAL MEDIA GALLERY
+              CREATIVE CAMPAIGNS GALLERY
             </span>
           </div>
           <h3 className="text-2xl sm:text-4xl font-black tracking-tight text-white uppercase font-sans">
-            Visual Feed & Social Grid
+            Visual Feed & Campaign Grid
           </h3>
+        </div>
+        <div className="text-xs font-mono text-white/50 bg-white/5 border border-white/10 px-3 py-1.5 rounded-full w-fit">
+          16 Featured Works
         </div>
       </div>
 
-      {/* NATURAL ASPECT RATIO MASONRY IMAGE GRID WITH ANIMATED EXPANSION */}
-      <motion.div 
-        layout
-        transition={{ duration: 0.5, ease: [0.16, 1, 0.3, 1] }}
-        className="flex gap-3 sm:gap-4 md:gap-5 items-start"
-      >
+      {/* NATURAL ASPECT RATIO MASONRY IMAGE GRID (ALL 16 VISIBLE) */}
+      <div className="flex gap-3 sm:gap-4 md:gap-5 items-start">
         {columns.map((colItems, colIdx) => (
           <div key={`social-col-${colIdx}`} className="flex-1 flex flex-col gap-3 sm:gap-4 md:gap-5 min-w-0">
-            {colItems.map(({ url, index }) => {
-              const isExtra = index >= INITIAL_VISIBLE_COUNT;
-              return (
-                <AnimatePresence key={`social-grid-item-${index}-${url}`} mode="popLayout">
-                  {(!isExtra || isExpanded) && (
-                    <motion.div
-                      layout
-                      initial={isExtra ? { opacity: 0, y: 50, scale: 0.95 } : false}
-                      animate={{ opacity: 1, y: 0, scale: 1 }}
-                      exit={{ 
-                        opacity: 0, 
-                        y: 50, 
-                        scale: 0.95,
-                        transition: { duration: 0.25, ease: "easeIn" } 
-                      }}
-                      transition={{
-                        duration: 0.45,
-                        delay: isExtra ? ((index - INITIAL_VISIBLE_COUNT) % 12) * 0.025 : 0,
-                        ease: [0.215, 0.61, 0.355, 1],
-                      }}
-                      className="group relative w-full rounded-xl sm:rounded-2xl overflow-hidden bg-[#121214] border border-white/10 hover:border-[#FE4A03]/80 transition-all duration-300 hover:shadow-[0_0_25px_rgba(254,74,3,0.3)] shrink-0"
-                    >
-                      <ProgressiveImage
-                        src={url}
-                        alt={`Social Shot ${index + 1}`}
-                        targetWidth={420}
-                        imgClassName="transition-transform duration-500 ease-out group-hover:scale-105"
-                      />
+            {colItems.map(({ url, index }) => (
+              <motion.div
+                key={`social-grid-item-${index}-${url}`}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  duration: 0.45,
+                  delay: (index % 8) * 0.04,
+                  ease: [0.215, 0.61, 0.355, 1],
+                }}
+                className="group relative w-full rounded-xl sm:rounded-2xl overflow-hidden bg-[#121214] border border-white/10 hover:border-[#FE4A03]/80 transition-all duration-300 hover:shadow-[0_0_25px_rgba(254,74,3,0.3)] shrink-0"
+              >
+                <ProgressiveImage
+                  src={url}
+                  alt={`Creative Campaign ${index + 1}`}
+                  targetWidth={500}
+                  imgClassName="transition-transform duration-500 ease-out group-hover:scale-105"
+                />
 
-                      {/* Subtle hover overlay */}
-                      <div className="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none z-10" />
-                    </motion.div>
-                  )}
-                </AnimatePresence>
-              );
-            })}
+                {/* Subtle hover overlay */}
+                <div className="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none z-10" />
+              </motion.div>
+            ))}
           </div>
         ))}
-      </motion.div>
-
-      {/* SEE MORE / SHOW LESS EXPANSION BUTTON */}
-      <div className="mt-8 sm:mt-10 flex flex-col items-center justify-center relative">
-        {!isExpanded && (
-          <div className="absolute -top-16 inset-x-0 h-20 bg-gradient-to-t from-[#0A0A0B] to-transparent pointer-events-none" />
-        )}
-        
-        <button
-          onClick={() => setIsExpanded(!isExpanded)}
-          className="group relative z-10 inline-flex items-center gap-2.5 px-6 py-3.5 rounded-full bg-[#FE4A03] hover:bg-[#ff5b1a] text-white font-bold text-sm sm:text-base tracking-wide transition-all duration-300 shadow-[0_0_25px_rgba(254,74,3,0.4)] hover:shadow-[0_0_35px_rgba(254,74,3,0.7)] cursor-pointer hover:scale-105 active:scale-95"
-        >
-          <span>{isExpanded ? "Show Less" : "See More"}</span>
-          {isExpanded ? (
-            <ChevronUp className="w-5 h-5 transition-transform duration-300 group-hover:-translate-y-0.5" />
-          ) : (
-            <ChevronDown className="w-5 h-5 transition-transform duration-300 group-hover:translate-y-0.5" />
-          )}
-        </button>
       </div>
     </motion.div>
   );
